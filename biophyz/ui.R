ddbbfdddadd37c8b9197c8b8ddfb33640c3a8f36
Aji John <aji.john@gmail.com>
2018-05-07 01:34:21 -0700

    Updated UI, horizontal fields, bounding box

--- biophyz/ui.R
+++ biophyz/ui.R
@@ -34,11 +34,25 @@ navbarPage("BioPhyz", id="nav",
                                                    c("Sun" = "sun",
                                                      "Shade" = "shade")),
                                       numericInput("sac", "% SA in contact with ground", 5),
-                                      numericInput("osab", "Organism Solar Absorptivity", 5),
-                                      numericInput("gsab", "Ground Solar Absorptivity", 5),
+                                      HTML("<div class='input-group'>
+                                        <input type='number' class='form-control' id='osab' placeholder='Organism solar absorptivity'/>
+                                        <span class='input-group-addon'>-</span>
+                                        <input type='number' class='form-control' id='gsab' placeholder='Ground solar absorptivity'/>
+                                        </div>"),
                                       conditionalPanel("input.organism == 'lizard'",
                                                        # Only prompt for svl when chosen Lizard
                                                    numericInput("svl", "Snout Vent Length(cm)", 5)
+                                      ),
+                                      conditionalPanel("input.organism == 'butterfly'",
+                                                       # Only prompt thorax specific inputs when chosen butterfly
+                                                       numericInput("td", "Thorax diameter(cm)", 5),
+                                                       numericInput("tt", "Thorax fur thickness(cm)", 5),
+                                                       numericInput("wsa", "Wing solar absorptivity", 5)
+
+                                      ),
+                                      conditionalPanel("input.organism == 'grasshopper'",
+                                                       # Only prompt for length when chosen grasshopper
+                                                       numericInput("glength", "Length(cm)", 5)
                                       ),
                                       conditionalPanel("input.organism == 'generic'",
                                                        radioButtons("shape", "Shape",
@@ -52,13 +66,22 @@ navbarPage("BioPhyz", id="nav",
                                                    c("Parallel" = "parallel",
                                                      "Transverse" = "tverse")),
 
-
+                                      HTML("<div class='input-group'>
+                                           <input type='text' class='form-control' id='Nlat' placeholder='N Lat'/>
+                                           <span class='input-group-addon'>-</span>
+                                           <input type='text' class='form-control' id='Slat' placeholder='S Lat'/>
+                                           </div>"),
+                                      HTML("<div class='input-group'>
+                                           <input type='text' class='form-control' id='Wlon' placeholder='W Lon'/>
+                                           <span class='input-group-addon'>-</span>
+                                           <input type='text' class='form-control' id='Elon' placeholder='E Lon'/>
+                                           </div>"),
                                       radioButtons("tf", "Timeframe:",inline = TRUE,
                                                    c("Past" = "past",
                                                      "Future" = "future")),
                                       br(),
                                       tags$head(
-                                        tags$style(HTML('#goDownload{background-color:orange}'))
+                                        tags$style(HTML('#goDownload{color: #fff;background-color:orange; border-color: #2e6da4}'))
                                       ),
                                       actionButton("goDownload", "Download", icon = icon("area-chart"))
 
